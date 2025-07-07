43389432160e383bf3422bbf25711edfc15711b1
AdrianCarrion <107324116+adrianC05@users.noreply.github.com>
2025-07-07 17:18:29 -0500

    Delete Front-end directory

--- Front-end/src/views/Dashboard.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import ShortDashboard from '@/components/ShortDashboard.vue'
-import { PageName } from '@/components/PageName';
-PageName.value = 'Dashboard'
-</script>
-
-<template>
-  <ShortDashboard/>
-</template>
